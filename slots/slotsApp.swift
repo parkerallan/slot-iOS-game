3082a94bbe3d2a34ff9cee6d0edde0246b8463ed
parkerallan <parkerallan2000@gmail.com>
2022-01-19 12:15:54 -0700

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by Parker Allan on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
